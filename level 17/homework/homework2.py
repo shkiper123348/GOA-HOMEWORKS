a8764efb8ef26ca9329bda9ef9c28b1314c70b69
shkiper123348 <qweqwe@gmail.com>
2024-08-21 16:44:20 +0400

    commit

---
+++ level 17/homework/homework2.py
@@ -0,0 +1,4 @@
+i = 50
+while i >= 10:
+    print(i)
+    i -= 1
---
+++ level 17/homework/homework5.py
@@ -0,0 +1,42 @@
+# for loop-ის 5 მაგალითი
+
+for i in range(5):
+    print ("my name is alex")
+
+for i in range(10):
+    print ("Goa Best")
+
+for i in range(i):
+    print (i)
+
+for i in range(5):
+    print ("my surname is chitanava")
+
+for i in range(10):
+    print (i*2)
+
+
+# while loop-ის 5 მაგელითი
+
+age = 9
+while age < i:
+    print ("you are small")
+
+i = 0
+while i < 5:
+    print("goa")
+    i = i + 1
+
+i = 3
+while i < 10:
+    print ("you have no coins")
+    i = i + 1
+
+num = 1
+while num < 19:
+    print("Nothing")
+    num = num + 1
+
+number = 5
+while number < 33:
+    print ("Goa")
